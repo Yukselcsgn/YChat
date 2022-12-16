da0542b5b9799ec9b8c49826dd01849025546a23
Yukselcsgn <yukselcosgun@hotmail.com.tr>
2022-12-16 17:15:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ychat/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.ychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        getSupportActionBar().hide();
+    }
+}
